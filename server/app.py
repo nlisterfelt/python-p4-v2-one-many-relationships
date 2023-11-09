fefe6dbd9e5919c91064b71926825549cbf58a76
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-11-08 20:41:30 -0600

    add one to many relationship

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ from flask import Flask
 from flask_migrate import Migrate
 
 from models import db, Employee, Onboarding, Review
-
+ 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
